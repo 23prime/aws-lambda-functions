ea19b6c25dcc166e6d13e660201b2af3a20e8bd1
23prime <23.prime.37@gmail.com>
2021-01-29 20:48:42 +0900

    Add push to Discord

--- spec/regular-push/lambda_function_spec.rb
+++ spec/regular-push/lambda_function_spec.rb
@@ -1,7 +1,15 @@
 require 'spec_helper'
 
 describe 'regular-push' do
-  it 'Correct request' do
-    expect(send_push_msg('MY_USER_ID')).to eq '200'
+  context 'LINE' do
+    it 'Correct request' do
+      expect(send_to_line('MY_USER_ID')).to eq '200'
+    end
+  end
+
+  context 'Discord' do
+    it 'Correct request' do
+      expect(send_to_discord).to eq '200'
+    end
   end
 end
--- src/regular-push/lambda_function.rb
+++ src/regular-push/lambda_function.rb
@@ -1,18 +1,27 @@
 require 'json'
 
+BASE_URI = ENV.fetch('GOKABOT_BASE_URI', nil)
+
 def lambda_handler(event:, context:)
-  send_push_msg('MY_USER_ID')
-  send_push_msg('NGA_GROUP_ID')
-  send_push_msg('KMT_GROUP_ID')
+  send_to_line('MY_USER_ID')
+  send_to_line('NGA_GROUP_ID')
+  send_to_line('KMT_GROUP_ID')
+  send_to_discord
 end
 
-def send_push_msg(target)
-  base_uri = ENV.fetch('GOKABOT_BASE_URI', nil)
+def send_to_line(target)
+  return 500 if BASE_URI.nil?
 
-  return 500 if base_uri.nil?
-
-  uri = URI.parse(base_uri + '/line/push/random')
+  uri = URI.parse("#{BASE_URI}/line/push/random")
   params = { target: target }
   response = Net::HTTP.post_form(uri, params)
+  return response.code
+end
+
+def send_to_discord
+  return 500 if BASE_URI.nil?
+
+  uri = URI.parse("#{BASE_URI}/discord/push/random")
+  response = Net::HTTP.post_form(uri, {})
   return response.code
 end
